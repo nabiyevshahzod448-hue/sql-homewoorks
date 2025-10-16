af810f2f3bb09abb4e83311af3872b046b6690c5
nabiyevshahzod448-hue <nabiyevshahzod448@gmail.com>
2025-10-15 22:58:51 -0700

    Update hw1.sql

--- lesson-1 /homework/hw1.sql
+++ lesson-1 /homework/hw1.sql
@@ -1,1 +1,7 @@
-
+Define the following terms: data, database, relational database, and table.
+List five key features of SQL Server.
+What are the different authentication modes available when connecting to SQL Server? (Give at least 2)
+Medium
+Create a new database in SSMS named SchoolDB.
+Write and execute a query to create a table called Students with columns: StudentID (INT, PRIMARY KEY), Name (VARCHAR(50)), Age (INT).
+Describe the differences between SQL Server
